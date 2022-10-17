0ff14699bb339b3a1eec3c15de778e8e94490180
Marsik <mara_ru88@mail.ru>
2022-10-17 13:17:39 +0300

    выполнили задание с четными числами

---
+++ DZ_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число");
+int number= int.Parse(Console.ReadLine() ?? "0");
+int num=1;
+while(num<=number)
+{
+    if(num%2==0) Console.Write(num + " ");
+    num +=1;
+}
