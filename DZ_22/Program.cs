1aa2e91fd280b21e3ad5704b8d05398ab5055985
Marsik <mara_ru88@mail.ru>
2022-10-17 12:00:29 +0300

    сделали вторую часть второго задания

---
+++ DZ_22/Program.cs
@@ -0,0 +1,21 @@
+﻿// / Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// // a = 2 b = 10 -> max = 10
+
+Console.WriteLine("Введите числа");
+int number_1= Convert.ToInt32(Console.ReadLine());
+int number_2= Convert.ToInt32(Console.ReadLine());
+int max= number_2;
+if(number_1> number_2)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+
+if (number_2> number_1)
+{
+    Console.WriteLine("Второе число больше первого");
+}
+else 
+{
+    Console.WriteLine(max= number_2);
+}
