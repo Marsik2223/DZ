b826e29c37891a8afb198c834793e212685a1402
Marsik <mara_ru88@mail.ru>
2022-10-17 11:44:18 +0300

    сделали третье часть первого задания

---
+++ DZ_13/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите числа");
+int number_1= Convert.ToInt32(Console.ReadLine());
+int number_2= Convert.ToInt32(Console.ReadLine());
+int number_3= Convert.ToInt32(Console.ReadLine());
+int max= number_1;
+if(number_2> number_1)
+{
+    Console.WriteLine(max= number_1);
+}
+
+if (number_3> number_1)
+{
+    Console.WriteLine(max= number_2);
+}
+else 
+{
+    Console.WriteLine(max= number_1);
+}
